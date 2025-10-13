765fd9564fe0128f7f120898b7110db00639b16f
kritithakur2 <159468995+kritithakur2@users.noreply.github.com>
2025-10-14 01:02:58 +0530

    Ready for deployment

--- client/src/lib/api/axios.js
+++ client/src/lib/api/axios.js
@@ -1,9 +1,18 @@
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
+import { getToken } from "../localstorage";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
 });
+axiosInstance.interceptors.request.use(
+	(config) => {
+		const token = getToken();
+		if (token) config.headers.authorization = `Bearer ${token}`;
+		return config;
+	},
+	(error) => Promise.reject(error),
+);
 
 export default axiosInstance
--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,6 +6,7 @@ import { ApiError } from "./errorHandler.js";
 export const authUser = asyncHandler(async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1] || req.cookies.token;
 
+  console.log(token)
   if (!token) throw new ApiError(401, "unauthorized access :-) token required");
 
   const decoded = await jwt.verify(token, process.env.SECRET_KEY);
